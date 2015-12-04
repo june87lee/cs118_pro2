594dc96000d7761eccf07b02712d0223acbdaa99
June Lee <june@Junes-MacBook-Pro.local>
2015-12-03 19:30:39 -0800

    please oh please 2

--- sender.c
+++ sender.c
@@ -64,9 +64,9 @@ int main(int argc, char *argv[])
      bzero((char *) &serv_addr, sizeof(serv_addr));
      //This is the source port number of server sender.
      portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+     cli_addr.sin_family = AF_INET;
+     cli_addr.sin_addr.s_addr = INADDR_ANY;
+     cli_addr.sin_port = htons(portno);
      //binds serv_addr to a particular socket request when executing
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
