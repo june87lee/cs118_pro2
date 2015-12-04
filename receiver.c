051545f2c84768bed0d37cb047d2e1761253ca15
Eric Chiang <eschiang2@gmail.com>
2015-12-03 21:02:53 -0800

    Update receiver.c

--- receiver.c
+++ receiver.c
@@ -13,19 +13,6 @@ Primary file for client.
 double LOSS_PROB = 0.00;
 double CORRUPT_PROB = 0.0;
 
-/*
- * error - wrapper for perror
- */
-void error(char * msg) {
-    perror(msg);
-    exit(1);
-}
-
-double random_num() {
-    return 1;
-    //return (double) rand()/(double) RAND_MAX;
-}
-
 int main(int argc, char * * argv) {
     int socketfd;
     int mSeqNo = 0;
@@ -33,15 +20,31 @@ int main(int argc, char * * argv) {
     struct pack rsp_pack;
     FILE * req_file; //file to ask for
     struct sockaddr_in serverAddr;
+    double loss;
+    double corr;
+    
     //zero out structs
     bzero((char * ) & rsp_pack, sizeof(rsp_pack));
     bzero((char * ) & rcv_pack, sizeof(rcv_pack));
     bzero((char * ) & serverAddr, sizeof(serverAddr));
     //check if correct arguments
-    if (argc != 4) {
-        fprintf(stderr, "usage: %s <hostname> <port> <filename>\n", argv[0]);
+    if (argc < 4 || argc > 6) {
+        fprintf(stderr, "usage: %s <hostname> <port> <filename> <loss prob> <corr prob>\n", argv[0]);
         exit(1);
     }
+    if(argc == 4){
+        loss = 0;
+        corr = 0;
+    }
+    else if(argc == 5){
+        loss = argv[4];
+        corr = 0;
+    }
+    else if(argc == 6){
+        loss = argv[4];
+        corr = argv[5];
+    }
+    
     //make socket
     socketfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (socketfd < 0)
@@ -64,43 +67,58 @@ int main(int argc, char * * argv) {
     rsp_pack.head.dPortNo = 0; //Unknown server port number
     rsp_pack.head.totalSize = sizeof(rsp_pack); //Total size of data to transmit
     rsp_pack.head.seqNo = mSeqNo;
-    strcpy(rsp_pack.data, argv[3]); //ask for file
+    char* fileName = argv[3];
+    strcpy(rsp_pack.data, fileName); //ask for file
+    rsp_pack.head.packSize = strlen(fileName);
     int sAddrLen = sizeof(serverAddr);
     //send request
     sendto(socketfd, & rsp_pack, sizeof(rsp_pack), 0, (struct sockaddr * ) & serverAddr, sAddrLen);
     bzero((char * ) & rsp_pack, sizeof(rsp_pack));
-    printf("Requested file %s\n", rsp_pack.data);
+    printf("Sent request for file %s\n", rsp_pack.data);
     srand(time(NULL));
     FILE * file = fopen(strcat(argv[3], "2"), "w");
-
     while (1) {
-        if (recvfrom(socketfd, & rcv_pack, sizeof(rcv_pack), 0, (struct sockaddr * ) & serverAddr, (socklen_t * ) & sAddrLen) < 0 || random_num() < LOSS_PROB) {
-            printf("Packet lost!\n");
-        } else if (random_num() < CORRUPT_PROB) {
-            printf("Packet corrupted!\n");
-            //if (sendto(socketfd, &rsp_pack, sizeof(rcv_pack), 0, (struct sockaddr*) &serverAddr, sAddrLen) < 0)
-            // error("ERROR responding to corrupt packet");
+        loss = rand()/(double) RAND_MAX;
+        corr = rand()/(double) RAND_MAX;
+        if (recvfrom(socketfd, & rcv_pack, sizeof(rcv_pack), 0, (struct sockaddr * ) & serverAddr, (socklen_t * ) & sAddrLen) < 0 || loss < LOSS_PROB) {
+            printf("Lost packet\n");
+        } else if (corr < CORRUPT_PROB) {
+            printf("Sending corrupted packet\n");
+            rsp_pack.head.sig = COR;
+            if (sendto(socketfd, &rsp_pack, sizeof(rcv_pack), 0, (struct sockaddr*) &serverAddr, sAddrLen) < 0)
+              error("ERROR responding to corrupt packet");
         } else {
-            printf("Received packet number %d\n", rcv_pack.head.seqNo);
-            if (rcv_pack.head.sig == CLO)
+            if (rcv_pack.head.sig == COR)
+                printf("Received corrupted packet");
+            else if (rcv_pack.head.sig == CLO)
                 break;
-            if (rcv_pack.head.sig == PACK && rcv_pack.head.seqNo == mSeqNo) {
-                printf("Good packet\n");
+            else if (rcv_pack.head.sig == PACK && rcv_pack.head.seqNo == mSeqNo) {
+                printf("Received packet number %d\n", rcv_pack.head.seqNo);
                 fwrite(rcv_pack.data, 1, rcv_pack.head.packSize, file);
-                printf("wrote \n");
                 rsp_pack.head.seqNo = rcv_pack.head.seqNo;
                 rsp_pack.head.sig = ACK;
                 rsp_pack.head.sPortNo = atoi(argv[2]);
                 rsp_pack.head.dPortNo = rcv_pack.head.sPortNo;
                 rsp_pack.head.totalSize = sizeof(rsp_pack);
+                rsp_pack.head.packSize = 0;
                 if (sendto(socketfd, & rsp_pack, sizeof(rsp_pack), 0, (struct sockaddr * ) & serverAddr, sAddrLen) < 0)
                     error("ERROR acking");
                 printf("ACK'd packet %d\n", rsp_pack.head.seqNo);
                 mSeqNo++;
-            }else
+            }
+            else
                 printf("IGNORE %d: expected %d\n", rcv_pack.head.seqNo, mSeqNo);
         }
     }
+    rsp_pack.head.seqNo = rcv_pack.head.seqNo;
+    rsp_pack.head.sig = CLO;
+    rsp_pack.head.sPortNo = atoi(argv[2]);
+    rsp_pack.head.dPortNo = rcv_pack.head.sPortNo;
+    rsp_pack.head.totalSize = sizeof(rsp_pack);
+    rsp_pack.head.packSize = 0;
+    if (sendto(socketfd, & rsp_pack, sizeof(rsp_pack), 0, (struct sockaddr * ) & serverAddr, sAddrLen) < 0)
+        error("ERROR acking");
+    printf("Sent FIN ACK\n");
     fclose(file);
     close(socketfd);
     return 0;
